f30eddb3663d061d1caabf5bb24832dbb4f356cf
CharithJaya <charithjc97@gmail.com>
2025-09-25 15:57:11 +0530

    Update MemberRegistrationForm.tsx

--- components/MemberRegistrationForm.tsx
+++ components/MemberRegistrationForm.tsx
@@ -40,7 +40,7 @@ const MemberRegistrationForm = () => {
     setMessage('');
 
     try {
-      const response = await fetch('http://localhost:8080/api/members', {
+      const response = await fetch('https://new-backend-oia8vq.fly.dev/members', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
